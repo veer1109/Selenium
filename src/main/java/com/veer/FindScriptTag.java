417f968a660d0400a6e9f00b8f4207f99feb9e61
veer1109 <veer.patil@ash-software.com>
2018-01-16 15:32:30 +0530

    Signed-off-by: veer1109 <veer.patil@ash-software.com>

--- src/main/java/com/veer/FindScriptTag.java
+++ src/main/java/com/veer/FindScriptTag.java
@@ -1,4 +1,30 @@
 package com.veer;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.List;
+
 public class FindScriptTag {
+    public static void main(String []arg) {
+        System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
+        WebDriver _driver = new ChromeDriver();
+        _driver.navigate().to("https://staging.villaplus.com/destinations/villas-in-spain/villas-in-mainland-spain/costa-del-sol/san-pedro-de-alcantara/villa-altavista-sol");
+
+        List<WebElement> tags = _driver.findElements(By.tagName("script"));
+
+        for (WebElement t: tags)
+        {
+
+            String htmlCode = (String) ((JavascriptExecutor) _driver).executeScript("return arguments[0].innerHTML;", t);
+            if(htmlCode.contains("GTMdataLayer")) {
+                System.out.println(" The tags is ");
+                System.out.println(htmlCode);
+                System.out.println("__________________________________");
+            }
+        }
+    }
 }
